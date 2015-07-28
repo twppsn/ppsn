c05625f2d0412843236180cf22b2be700bfe1268
neolithos <neolithos@outlook.com>
2015-07-28 14:28:13 +0200

    First implementation for the content owner

---
+++ PPSnClient/Shell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Neo.IronLua;
+
+namespace TecWare.PPSn
+{
+	///////////////////////////////////////////////////////////////////////////////
+	/// <summary></summary>
+	public interface IPpsShell
+	{
+		/// <summary>Synchronisation</summary>
+		SynchronizationContext Context { get; }
+		/// <summary>Access to the current lua engine.</summary>
+		Lua Lua { get; }
+	} // interface IPpsShell
+}
