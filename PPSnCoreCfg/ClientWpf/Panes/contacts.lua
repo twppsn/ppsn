590c98e7446bd2d37ad451fa346667453d1e3c92
neolithos <neolithos@outlook.com>
2016-08-11 11:39:39 +0200

    Add: Sep-Script

---
+++ PPSnCoreCfg/ClientWpf/Panes/contacts.lua
@@ -0,0 +1,73 @@
+﻿
+--ViewAdre = getView(Data:Head:First:AdreHead);
+
+--[[
+pushContact = command(
+    function (args) : void
+    	do (trans = UndoManager:BeginTransaction('Neue Adresse'))
+    		
+    		local ansp = getView(tvw:SelectedItem.AnspAdre);
+    		local newRow = ansp:Add { Name = "Test4" };
+			
+			local tviParent = tvw:ItemContainerGenerator:ContainerFromItem(tvw:SelectedItem);
+			tviParent.IsExpanded = true;
+			
+			local tviNew = tviParent:ItemContainerGenerator:ContainerFromItem(newRow);
+			if tviNew ~= nil then
+				tviNew.IsExpanded = true;
+				tviNew.IsSelected = true;
+			else
+				msgbox("tvi is null");
+			end;			
+
+            trans:Commit();
+        end;
+        
+        -- PushDataAsync();
+    end
+);
+]]
+
+
+		
+pushContact = command(
+    function (args) : void
+        runTask(PushDataAsync());
+    end
+);
+
+newAddress = command(
+    function (args) : void
+	    do (trans = UndoManager:BeginTransaction("Neue Adresse"))
+			local viewAdre = getView(Data:Head:First:AdreHead);
+			viewAdre:Add({ Name = "Neue Adresse"});
+			
+ 			trans:Commit();
+		end;
+    end
+);
+		
+newPartner = command(
+    function (args) : void
+  		local cur = ADR_TreeView:SelectedValue;
+        if cur ~= nil and cur:Table:TableName == "Adre" then
+			do (trans = UndoManager:BeginTransaction("Neuer Partner"))
+ 			    local viewAdre = getView(cur.AnspAdre);
+ 			    viewAdre:Add({ Name = "Neuer Partner"});
+				trans:Commit();
+			end;
+		end;
+    end
+);
+		
+delItem = command(
+    function (args) : void
+  		local cur = ADR_TreeView:SelectedValue;
+        if cur ~= nil then
+			do (trans = UndoManager:BeginTransaction("Neuer Partner"))
+ 			    cur:Remove({"Löschen " .. (cur.Name)});
+				trans:Commit();
+			end;
+		end;
+    end
+);
