73004ace64319428dd660b5f92c51d5a32f6bc06
WH <waldemar.hermann@outlook.com>
2016-08-26 12:12:15 +0200

    add: Dataset offer

---
+++ PPSnCoreCfg/ClientWpf/Panes/offer.lua
@@ -0,0 +1,17 @@
+﻿
+pushContract = command(
+    function (args) : void
+        runTask(PushDataAsync());
+    end
+);
+
+newPosition = command(
+    function (args) : void
+	    do (trans = UndoManager:BeginTransaction("Neue Position"))
+			local viewAnpo = getView(Data:Head:First:AnpoHead);
+			viewAnpo:Add({ Text = "Neue Position"});
+ 			trans:Commit();
+		end;
+    end
+);
+
