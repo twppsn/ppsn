850c9e0074dc925332561928d8276a7e9dd70997
neolithos <neolithos@outlook.com>
2017-03-08 11:40:00 +0100

    Add: SecureStringCompare

---
+++ PPSnCore/Stuff/Strings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecWare.PPSn.Stuff
+{
+	public static partial class PpsProcs
+	{
+		public unsafe static bool SecureStringCompare(SecureString ss1, SecureString ss2)
+		{
+			var bstr1 = IntPtr.Zero;
+			var bstr2 = IntPtr.Zero;
+			try
+			{
+				bstr1 = Marshal.SecureStringToBSTR(ss1);
+				bstr2 = Marshal.SecureStringToBSTR(ss2);
+				var length1 = *((int*)bstr1.ToPointer() - 1);
+				var length2 = *((int*)bstr2.ToPointer() - 1);
+				
+				if (length1 == length2)
+				{
+					var c1 = (char*)bstr1.ToPointer();
+					var c2 = (char*)bstr2.ToPointer();
+					var c1end = (char*)((byte*)bstr1.ToPointer() + length1);
+					var c2end = (char*)((byte*)bstr2.ToPointer() + length1);
+
+					var r = true;
+					while (c1 < c1end)
+					{
+						r = r && *c1 == *c2;
+						c1++;
+						c2++;
+					}
+
+					return r;
+				}
+				else
+					return false;
+			}
+			finally
+			{
+				if (bstr2 != IntPtr.Zero)
+					Marshal.ZeroFreeBSTR(bstr2);
+				if (bstr1 != IntPtr.Zero)
+					Marshal.ZeroFreeBSTR(bstr1);
+			}
+		}
+	}
+}
---
+++ PPSnCoreTest/Stuff.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using TecWare.PPSn.Stuff;
+
+namespace TecWare.PPSn
+{
+	[TestClass]
+	public class StuffTests
+	{
+
+		[TestMethod]
+		public unsafe void CompareSecureString()
+		{
+			var strs = new string[] { "Hallo Welt", "Hallo Welt" };
+
+			fixed (char* s1 = strs[0])
+			fixed(char* s2 = strs[1])
+			{
+				var ss1 = new SecureString(s1, strs[0].Length);
+				var ss2 = new SecureString(s2, strs[1].Length);
+
+				Assert.IsTrue(PpsProcs.SecureStringCompare(ss1, ss2));
+
+				ss1.Dispose();
+				ss2.Dispose();
+			}
+		}
+	}
+}
