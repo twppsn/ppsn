de003d12eec2a56d9b02e98376db279ec1a44766
neolithos <neolithos@outlook.com>
2017-03-08 11:45:53 +0100

    Chg: CompareSecureString moved

--- PPSnDesktop/UI/PpsSplashWindow.xaml.cs
+++ PPSnDesktop/UI/PpsSplashWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
 using TecWare.PPSn.Data;
+using TecWare.PPSn.Stuff;
 
 // todo: designer find place for ErrorText in ui
 
@@ -294,16 +295,15 @@ namespace TecWare.PPSn.UI
 					using (var plc = new PpsClientLogin(loginStateUnSafe.CurrentEnvironment.Uri.ToString(), "", false))
 					{
 						var newCreds = (NetworkCredential)loginStateUnSafe.GetCredentials();
-						if (newCreds != null && plc != null)
-							if (plc.UserName != newCreds.UserName || !SecureStringCompare(plc.GetPassword(), newCreds.SecurePassword))
-
-								if (MessageBox.Show("Kennwort speichern?", "PPSn", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-								{
-									plc.UserName = newCreds.UserName;
-									plc.SetPassword(newCreds.SecurePassword);
-									plc.Save = true;
-									plc.Commit();
-								}
+						if (newCreds != null && plc != null
+							&& (plc.UserName != newCreds.UserName || !PpsProcs.SecureStringCompare(plc.GetPassword(), newCreds.SecurePassword))
+							&& MessageBox.Show("Kennwort speichern?", "PPSn", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+						{
+							plc.UserName = newCreds.UserName;
+							plc.SetPassword(newCreds.SecurePassword);
+							plc.Save = true;
+							plc.Commit();
+						}
 					}
 
 					return new Tuple<PpsEnvironmentInfo, ICredentials>(loginStateUnSafe.CurrentEnvironment, loginStateUnSafe.GetCredentials());
@@ -317,38 +317,6 @@ namespace TecWare.PPSn.UI
 				SetValue(statusPaneVisiblePropertyKey, Visibility.Visible);
 			}
 		} // proc ShowLogin
-
-		public static bool SecureStringCompare(SecureString ss1, SecureString ss2)
-		{
-			var bstr1 = IntPtr.Zero;
-			var bstr2 = IntPtr.Zero;
-			try
-			{
-				bstr1 = Marshal.SecureStringToBSTR(ss1);
-				bstr2 = Marshal.SecureStringToBSTR(ss2);
-				var length1 = Marshal.ReadInt32(bstr1, -4);
-				var length2 = Marshal.ReadInt32(bstr2, -4);
-				if (length1 == length2)
-				{
-					for (int x = 0; x < length1; ++x)
-					{
-						var b1 = Marshal.ReadByte(bstr1, x);
-						var b2 = Marshal.ReadByte(bstr2, x);
-						if (b1 != b2)
-							return false;
-					}
-				}
-				else return false;
-				return true;
-			}
-			finally
-			{
-				if (bstr2 != IntPtr.Zero)
-					Marshal.ZeroFreeBSTR(bstr2);
-				if (bstr1 != IntPtr.Zero)
-					Marshal.ZeroFreeBSTR(bstr1);
-			}
-		}
 
 		public async Task<Tuple<PpsEnvironmentInfo, ICredentials>> ShowLoginAsync(PpsEnvironmentInfo selectEnvironment)
 		{
