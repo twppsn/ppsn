c5be898d82b72e6932b12ce5e62e6c1ae796d414
rkblackfire <krug@tecware-gmbh.de>
2017-04-28 10:11:07 +0200

    add: files

---
+++ PPSnDesktopUI/UI/PpsAttachmentsControl.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using TecWare.PPSn.Data;
+
+namespace TecWare.PPSn.UI
+{
+	public interface IPpsAttachmentItem
+	{
+		bool Remove();
+	} // interface IPpsAttachmentItem
+
+	public interface IPpsAttachments : IEnumerable<IPpsAttachmentItem>
+	{
+	} // interface IPpsAttachments
+
+	public interface IPpsAttachmentSource
+	{
+		IPpsAttachments Attachments { get; }
+	} // interface IPpsAttachmentSource
+
+	  /// <summary>
+	  /// Interaction logic for PpsAttachmentsControl.xaml
+	  /// </summary>
+	public partial class PpsAttachmentsControl : UserControl, IPpsAttachmentSource
+	{
+		public PpsAttachmentsControl()
+		{
+			InitializeComponent();
+
+			CommandBindings.Add(
+				new CommandBinding(RemoveAttachmentCommand,
+					(sender, e) =>
+					{
+						var itm = (PpsDataRow)GetValue(AttachmentListSelectedItemProperty);
+						var list = (PpsDataRelatedFilterDesktop)GetValue(AttachmentListProperty);
+						list.Remove(itm);
+						e.Handled = true;
+					},
+					(sender, e) => e.CanExecute = true
+				)
+			);
+		}
+		
+		public static  DependencyProperty AttachmentListProperty =
+			DependencyProperty.Register("AttachmentList", 
+			typeof(PpsDataRelatedFilterDesktop),
+			typeof(PpsAttachmentsControl));
+		public static DependencyProperty AttachmentListSelectedItemProperty =
+			DependencyProperty.Register("AttachmentListSelectedItem",
+			typeof(PpsDataRow),
+			typeof(PpsAttachmentsControl));
+		public static RoutedUICommand RemoveAttachmentCommand { get; } = new RoutedUICommand("RemoveAttachment", "RemoveAttachment", typeof(PpsAttachmentsControl));
+
+		public IPpsAttachments Attachments { get; } = null; // wird von ItemsSource abgeleitet
+
+
+	}
+}
