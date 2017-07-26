e9029aea5624b80faa2cdad153a944355cf48866
WH <waldemar.hermann@outlook.com>
2017-07-26 17:38:14 +0200

    chg: load Offer + Contract

--- PPSnCoreCfg/sds/offer/offerServer.lua
+++ PPSnCoreCfg/sds/offer/offerServer.lua
@@ -1,4 +1,5 @@
-﻿
+﻿const OfferType = 'sdsOffer';
+
 function GetNextNumber(trans, dataset) : string
 	
 	local row = trans:ExecuteSingleRow{
@@ -7,3 +8,64 @@ function GetNextNumber(trans, dataset) : string
 	
 	return ((row[0] or 0) + 1):ToString("00000");
 end;
+
+function GetOfferData(obj, ds)
+
+	local trans = Db.Main;
+	do (t = ds:BeginData())
+
+		-- Kopfdaten --
+		ds:Head:AddRange(
+			trans:ExecuteSingleResult{
+				select = [[
+					dbo.ObjK
+						=sds.Anko[Objk.Id = Anko.ObjkId]
+				]],
+				columnList = ds:Head,
+				[1] = { Id = obj.Id, Typ = OfferType }
+			}
+		);
+
+		if #ds:Head == 0 then
+			error("Angebot nicht gefunden '{1},{0}:{2}":Format(obj.Id, obj.Nr, obj.Guid));
+		end;
+
+		-- Positionen --
+		ds:Anpo:AddRange(
+			trans:ExecuteSingleResult{
+				select = "sds.Anpo",
+				columnList = ds:Anpo,
+				[1] = { ObjKId = obj.Id }
+			}
+		);
+
+		ds:Commit();
+	end;
+end;
+
+function mergeOfferToSql(obj, data)
+
+	trans = Db.Main;
+
+	-- write anko table
+	trans:ExecuteNoneResult {
+		upsert ="sds.Anko",
+		rows = data:Head
+	};
+
+	-- write anpo
+	trans:ExecuteNoneResult {
+		upsert ="sds.Anpo",
+		rows = data:Anpo
+	};
+	
+end;
+
+-- overwrite NextNumber
+NextNumber = GetNextNumber;
+
+-- auto merge data
+OnAfterPush["sds.offer"] = mergeOfferToSql;
+
+-- get offer data
+OnCreateRevision["sds.offer"] = GetOfferData;
