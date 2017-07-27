caa7cb85919c48eb60a4e2391ece944cf054ed95
schmidt <schmidt@tecware-gmbh.de>
2017-07-27 16:07:03 +0200

    changed: removed unused BoolToVisibilityConverter()

--- PPSnDesktopUI/UI/PpsTracePane.xaml.cs
+++ PPSnDesktopUI/UI/PpsTracePane.xaml.cs
@@ -307,19 +307,4 @@ namespace TecWare.PPSn.UI
 	} // class ExceptionToPropertyConverter
 
 	#endregion
-
-	public class BoolToVisibilityConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			if ((bool)value)
-				return Visibility.Visible;
-			else return Visibility.Collapsed;
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotSupportedException();
-		}
-	}
 }
