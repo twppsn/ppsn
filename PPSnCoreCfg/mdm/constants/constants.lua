e4f98a0468f6c3222d33a56913822521d35cc102
neolithos <neolithos@outlook.com>
2017-08-01 12:36:46 +0200

    Add: Definition for countries

---
+++ PPSnCoreCfg/mdm/constants/constants.lua
@@ -0,0 +1,58 @@
+﻿
+-------------------------------------------------------------------------------
+-- Registers the constant in the object table
+function RegisterConstant(table)
+
+	-- check table arguments
+	assert(table.Guid, "Guid is empty.");
+	assert(table.Nr, "Nr is empty.");
+	assert(table.Typ, "Typ is empty.");
+	assert(table.Name, "Name is empty.");
+
+	RegisterInitializationAction(
+		20000, "RegisterConstant",
+		function () : void
+			do (ctx = ImpersonateSystem())
+				local objData = {
+					Guid = table.Guid,
+					Nr = table.Nr,
+					Typ = table.Typ,
+					MimeType = "text/dataset",
+					IsRev = true
+				};
+
+				Db.Main.ExecuteNoneResult {
+					upsert = "dbo.ObjK",
+					on = { "Guid" },
+					objData
+				};
+
+				local args = {
+					upsert = "dbo.ObjT",
+					on = { "Key", "UserId", "Class" },
+					{
+						ObjKId = objData.Id,
+						Key = "Name",
+						Class = 0,
+						Value = table.Name,
+						UserId = 0
+					}
+				};
+
+				if table.Comment then
+					args[2] = {
+						ObjKId = objData.Id,
+						Key = "Comment",
+						Class = 0,
+						Value = table.Comment,
+						UserId = 0
+					}
+				end;
+
+				Db.Main.ExecuteNoneResult(args);
+
+				await(ctx:CommitAsync());
+			end;
+		end
+	);
+end;
---
+++ PPSnCoreCfg/mdm/constants/countries.lua
@@ -0,0 +1,33 @@
+﻿const Guid typeof System.Guid;
+
+
+-- Register
+RegisterConstant {
+	Guid = Guid:Parse("{7bc6d439-4b63-49a2-88c9-0856c55e783e}"),
+	Nr = "LAND",
+	Typ = "mdmCountries",
+	Name = "Ländereinstellungen"
+};
+
+
+-- Create a revision
+OnCreateRevision["mdmCountries"] = function (obj, data)
+
+	data:Land:AddRange(
+		Db.Main:ExecuteSingleResult {
+			select = "dbo.Land",
+			columnList = data:Land
+		}
+	);
+
+end;
+
+-- Create update LAND
+OnAfterPush["mdmCountries"] = function (obj, data)
+
+	Db.Main.ExecuteNoneResult {
+		upsert = "dbo.Land",
+		rows = data:Land
+	};
+
+end;
