0efd947a1e385eb8756b7cb8181b671a2db39771
WH <waldemar.hermann@outlook.com>
2017-08-01 14:33:00 +0200

    add: System User

---
+++ PPSnMaster/Database/SystemUser.sql
@@ -0,0 +1,7 @@
+﻿-- User "System" muss vorhanden sein
+IF NOT EXISTS (SELECT * FROM dbo.[User] WHERE Id = 0)
+BEGIN
+	SET IDENTITY_INSERT dbo.[User] ON;
+	INSERT INTO dbo.[User] (Id, [Login]) VALUES (0, 'System');
+	SET IDENTITY_INSERT dbo.[User] OFF;
+END
