e25a44f09a8b4563a232d0819518435c1a17ed69
Neolithos <neolithos@outlook.com>
2017-09-06 12:23:17 +0200

    Fix: Style

--- PPSnDesktopUI/Controls/ComboBoxBehavior.cs
+++ PPSnDesktopUI/Controls/ComboBoxBehavior.cs
@@ -1,8 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region -- copyright --
+//
+// Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+// European Commission - subsequent versions of the EUPL(the "Licence"); You may
+// not use this work except in compliance with the Licence.
+//
+// You may obtain a copy of the Licence at:
+// http://ec.europa.eu/idabc/eupl
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+// specific language governing permissions and limitations under the Licence.
+//
+#endregion
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -10,69 +20,47 @@ using System.Windows.Input;
 
 namespace TecWare.PPSn.Controls
 {
-	///////////////////////////////////////////////////////////////////////////////
+	#region -- class PpsSelectionChangedBehavior --------------------------------------
+
 	/// <summary></summary>
-	public class PPSnSelectionChangedBehavior
+	public static class PpsSelectionChangedBehavior
 	{
-		public static readonly DependencyProperty CommandProperty = DependencyProperty.RegisterAttached("Command",
-			typeof(ICommand),
-			typeof(PPSnSelectionChangedBehavior),
-			new PropertyMetadata(PropertyChangedCallback));
+		public static readonly DependencyProperty CommandProperty = DependencyProperty.RegisterAttached("Command", typeof(ICommand), typeof(PpsSelectionChangedBehavior), new FrameworkPropertyMetadata(PropertyChangedCallback));
 
 		public static void PropertyChangedCallback(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
 		{
-			var selector = depObj as Selector;
-			if (selector == null)
-				return;
-
-			selector.SelectionChanged += new SelectionChangedEventHandler(SelectionChanged);
-		}
+			if (depObj is Selector selector)
+				selector.SelectionChanged += new SelectionChangedEventHandler(SelectionChanged);
+		} // proc PropertyChangedCallback
 
 		public static ICommand GetCommand(UIElement element)
-		{
-			return (ICommand)element.GetValue(CommandProperty);
-		}
+			=> (ICommand)element.GetValue(CommandProperty);
 
 		public static void SetCommand(UIElement element, ICommand command)
-		{
-			element.SetValue(CommandProperty, command);
-		}
-
+			=> element.SetValue(CommandProperty, command);
+		
 		private static void SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			var selector = sender as Selector;
-			if (selector == null)
-				return;
+			if (sender is Selector selector && selector.GetValue(CommandProperty) is ICommand command)
+				command.Execute(selector.SelectedItem);
+		} // proc SelectionChanged
+	} // class PpsSelectionChangedBehavior
 
-			var command = selector.GetValue(CommandProperty) as ICommand;
-			if (command == null)
-				return;
+	#endregion
 
-			command.Execute(selector.SelectedItem);
-		}
-	} // class PPSnSelectionChangedBehavior
+	#region -- class PpsComboBoxBehavior ----------------------------------------------
 
-	///////////////////////////////////////////////////////////////////////////////
 	/// <summary></summary>
-	public class PPSnComboBoxBehavior
+	public static class PpsComboBoxBehavior
 	{
+		public static readonly DependencyProperty IsNullableProperty = DependencyProperty.RegisterAttached("IsNullable", typeof(bool), typeof(PpsComboBoxBehavior), new FrameworkPropertyMetadata(false));
+
 		public static object GetIsNullable(ComboBox comboBox)
-		{
-			return (bool)comboBox.GetValue(IsNullableProperty);
-		}
-
+			=> (bool)comboBox.GetValue(IsNullableProperty);
+		
 		public static void SetIsNullable(ComboBox comboBox, object value)
-		{
-			comboBox.SetValue(IsNullableProperty, value);
-		}
+			=> comboBox.SetValue(IsNullableProperty, value);
+	} // class PpsComboBoxBehavior
 
-		public static readonly DependencyProperty IsNullableProperty =
-			DependencyProperty.RegisterAttached(
-				"IsNullable",
-				typeof(bool),
-				typeof(PPSnComboBoxBehavior),
-				new PropertyMetadata(false)
-				);
-	} // class PPSnComboBoxBehavior
-
+	#endregion
 }
--- PPSnDesktopUI/Controls/ReadOnlyPaneBehavior.cs
+++ PPSnDesktopUI/Controls/ReadOnlyPaneBehavior.cs
@@ -1,45 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region -- copyright --
+//
+// Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+// European Commission - subsequent versions of the EUPL(the "Licence"); You may
+// not use this work except in compliance with the Licence.
+//
+// You may obtain a copy of the Licence at:
+// http://ec.europa.eu/idabc/eupl
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+// specific language governing permissions and limitations under the Licence.
+//
+#endregion
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 
 namespace TecWare.PPSn.Controls
 {
-	///////////////////////////////////////////////////////////////////////////////
+	#region -- class PpsReadOnlyPaneBehavior ------------------------------------------
+
 	/// <summary></summary>
-	public class PPSnReadOnlyPaneBehavior
+	public static class PpsReadOnlyPaneBehavior
 	{
 		//From MSDN:
 		//Note, however, that designating a property as inheritable does have some performance considerations.
 		//In cases where that property does not have an established local value, or a value obtained through styles, templates, or data binding,
 		//an inheritable property provides its assigned property values to all child elements in the logical tree
-		public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.RegisterAttached(
-			"IsReadOnly",
-			typeof(bool), 
-			typeof(PPSnReadOnlyPaneBehavior),
-			new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits, ReadOnlyPropertyChanged));
+		public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.RegisterAttached("IsReadOnly", typeof(bool), typeof(PpsReadOnlyPaneBehavior), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits, ReadOnlyPropertyChanged));
 
 		public static bool GetIsReadOnly(DependencyObject o)
-		{
-			return (bool)o.GetValue(IsReadOnlyProperty);
-		}
+			=> (bool)o.GetValue(IsReadOnlyProperty);
 
 		public static void SetIsReadOnly(DependencyObject o, bool value)
-		{
-			o.SetValue(IsReadOnlyProperty, value);
-		}
-
+			=> o.SetValue(IsReadOnlyProperty, value);
+		
 		private static void ReadOnlyPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
 		{
-			if (o is TextBox)
-			{
-				((TextBox)o).IsReadOnly = (bool)e.NewValue;
-			}
-		}
-	} // class PPSnReadOnlyPaneBehavior
+			if (o is TextBox txt)
+				txt.IsReadOnly = (bool)e.NewValue;
+		} // proc ReadOnlyPropertyChanged
+	} // class PpsReadOnlyPaneBehavior
+
+	#endregion
 }
