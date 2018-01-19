504069170963cd4cbccebce8fa6d3dcb224aedf6
neolithos <neolithos@outlook.com>
2018-01-19 09:41:21 +0100

    Add: User<->Pers

--- PPSnMod/Sql/tsql/ServerLogins.sql
+++ PPSnMod/Sql/tsql/ServerLogins.sql
@@ -1,10 +1,12 @@
 ﻿SELECT 
 		u.Id			AS Id
-		,s.Name			AS Name
+		,ISNULL(p.Name, s.name) AS Name
 		,u.[Security]	AS [Security]
 		,u.LoginVersion	AS LoginVersion
 		,s.[type]		AS LoginType
 		,s.name			AS [Login]
 		,CASE type WHEN 'S' THEN loginproperty(s.name, 'PasswordHash') ELSE NULL END AS LoginHash
-	FROM dbo.[User] AS u INNER JOIN
-		sys.server_principals AS s ON s.name COLLATE database_default = u.[Login] COLLATE database_default AND s.[type] IN ('S', 'U') AND s.is_disabled = 0
+		,p.Id			AS [PersId]
+	FROM dbo.[User] AS u 
+		INNER JOIN sys.server_principals AS s ON (s.name COLLATE database_default = u.[Login] COLLATE database_default AND s.[type] IN ('S', 'U') AND s.is_disabled = 0)
+		LEFT OUTER JOIN dbo.Pers p ON (u.PersId = p.Id)
