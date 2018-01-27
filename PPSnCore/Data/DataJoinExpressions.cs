7dac2db8d28e7556100c07e2d310586f2eec90ef
neolithos <neolithos@outlook.com>
2018-01-27 13:54:13 +0100

    Chg: Style and comments

--- PPSnCore/Data/DataJoinExpressions.cs
+++ PPSnCore/Data/DataJoinExpressions.cs
@@ -19,60 +19,90 @@ using TecWare.DE.Data;
 
 namespace TecWare.PPSn.Data
 {
-	#region -- enum PpsDataJoinType -----------------------------------------------------
+	#region -- enum PpsDataJoinType ---------------------------------------------------
 
 	/// <summary>Join type</summary>
 	public enum PpsDataJoinType
 	{
+		/// <summary>Not specified.</summary>
 		None,
+		/// <summary>Inner join</summary>
 		Inner,
+		/// <summary>Left outer join</summary>
 		Left,
+		/// <summary>Right outer join</summary>
 		Right
 	} // enum JoinType
 
 	#endregion
 
-	#region -- class PpsJoinException ---------------------------------------------------
+	#region -- class PpsJoinException -------------------------------------------------
 
+	/// <summary>Join syntax exception.</summary>
 	public class PpsJoinException : FormatException
 	{
 		private readonly int position;
 
+		/// <summary>Join syntax exception.</summary>
+		/// <param name="position">Position of the syntax error.</param>
+		/// <param name="message"></param>
+		/// <param name="innerException"></param>
 		public PpsJoinException(int position, string message, Exception innerException = null)
 			: base(message, innerException)
 		{
 			this.position = position;
 		} // ctor
 
+		/// <summary>Position of the syntax error.</summary>
 		public int Position => position;
 	} // class PpsJoinException
 
 	#endregion
 
-	#region -- class PpsJoinExpression --------------------------------------------------
+	#region -- class PpsJoinExpression ------------------------------------------------
 
+	/// <summary>Join expression</summary>
+	/// <typeparam name="TTABLE">Table information type.</typeparam>
 	public abstract class PpsDataJoinExpression<TTABLE>
 		where TTABLE : IDataColumns
 	{
+		#region -- class PpsExpressionPart --------------------------------------------
+
+		/// <summary>Base part of the expression.</summary>
 		public abstract class PpsExpressionPart
 		{
 		} // class PpsExpressionPart
 
+		#endregion
+
+		#region -- class PpsTableExpression -------------------------------------------
+
+		/// <summary>Table part of the expression</summary>
 		public sealed class PpsTableExpression : PpsExpressionPart
 		{
 			private readonly TTABLE table;
 			private readonly string alias;
 
+			/// <summary></summary>
+			/// <param name="table"></param>
+			/// <param name="alias"></param>
 			public PpsTableExpression(TTABLE table, string alias)
 			{
 				this.table = table;
 				this.alias = alias;
 			} // ctor
 
+			/// <summary>Referenced table.</summary>
 			public TTABLE Table => table;
+			/// <summary>Alias of this table.</summary>
 			public string Alias => alias;
 		} // class PpsTableExpression
 
+		#endregion
+
+		#region -- class PpsJoinExpression --------------------------------------------
+
+		/// <summary>Join part of the expression.</summary>
 		public sealed class PpsJoinExpression : PpsExpressionPart
 		{
 			private readonly PpsExpressionPart left;
@@ -81,25 +111,53 @@ namespace TecWare.PPSn.Data
 
 			private readonly string onStatement;
 
+			/// <summary></summary>
+			/// <param name="left"></param>
+			/// <param name="type"></param>
+			/// <param name="right"></param>
+			/// <param name="onStatement"></param>
 			public PpsJoinExpression(PpsExpressionPart left, PpsDataJoinType type, PpsExpressionPart right, string onStatement)
 			{
-				this.left = left;
+				this.left = left ?? throw new ArgumentNullException(nameof(left));
 				this.type = type;
-				this.right = right;
+				this.right = right ?? throw new ArgumentNullException(nameof(right));
 				this.onStatement = onStatement ?? throw new ArgumentNullException(nameof(onStatement));
 			} // ctor
 
+			/// <summary>Left site of the join.</summary>
 			public PpsExpressionPart Left => left;
+			/// <summary>Right site of the join.</summary>
 			public PpsExpressionPart Right => right;
+			/// <summary>Join statement.</summary>
 			public string Statement => onStatement;
+			/// <summary>Type</summary>
 			public PpsDataJoinType Type => type;
 		} // class PpsJoinExpression
 
+		#endregion
+
+		#region -- class PpsJoinVisitor<TRESULT> --------------------------------------
+
+		/// <summary>Visitor pattern for join expressions.</summary>
+		/// <typeparam name="TRESULT"></typeparam>
 		public abstract class PpsJoinVisitor<TRESULT>
 		{
+			/// <summary>Visit table statement</summary>
+			/// <param name="table"></param>
+			/// <param name="alias"></param>
+			/// <returns></returns>
 			public abstract TRESULT CreateTableStatement(TTABLE table, string alias);
+			/// <summary>Visit join statement</summary>
+			/// <param name="leftExpression"></param>
+			/// <param name="type"></param>
+			/// <param name="rightExpression"></param>
+			/// <param name="on"></param>
+			/// <returns></returns>
 			public abstract TRESULT CreateJoinStatement(TRESULT leftExpression, PpsDataJoinType type, TRESULT rightExpression, string on);
 
+			/// <summary>Visit expression part</summary>
+			/// <param name="expr"></param>
+			/// <returns></returns>
 			public TRESULT Visit(PpsExpressionPart expr)
 			{
 				switch(expr)
@@ -113,11 +171,16 @@ namespace TecWare.PPSn.Data
 				}
 			} // func Visit
 
+			/// <summary>Visit expression part</summary>
+			/// <param name="expr"></param>
+			/// <returns></returns>
 			public TRESULT Visit(PpsDataJoinExpression<TTABLE> expr)
 				=> Visit(expr.root);
 		} // class PpsJoinVisitor
 
-		#region -- class SimpleParser -----------------------------------------
+		#endregion
+
+		#region -- class SimpleParser -------------------------------------------------
 
 		private sealed class SimpleParser
 		{
@@ -268,6 +331,7 @@ namespace TecWare.PPSn.Data
 
 		private PpsExpressionPart root;
 
+		/// <summary></summary>
 		public PpsDataJoinExpression()
 		{
 		} // ctor
@@ -284,10 +348,20 @@ namespace TecWare.PPSn.Data
 		private PpsExpressionPart CreateTable(string tableName, string tableAlias)
 			=> new PpsTableExpression(ResolveTable(tableName), tableAlias);
 
+		/// <summary>Create on statement</summary>
+		/// <param name="left"></param>
+		/// <param name="joinOp"></param>
+		/// <param name="right"></param>
+		/// <returns></returns>
 		protected abstract string CreateOnStatement(PpsTableExpression left, PpsDataJoinType joinOp, PpsTableExpression right);
 
+		/// <summary>Resolve table by name.</summary>
+		/// <param name="tableName"></param>
+		/// <returns></returns>
 		protected abstract TTABLE ResolveTable(string tableName);
 
+		/// <summary>Parse expression.</summary>
+		/// <param name="expression"></param>
 		public void Parse(string expression)
 		{
 			// t1 alias
