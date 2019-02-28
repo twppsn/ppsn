bcc57e26f6114a7aeb37a57f057cea16808ca0f5
neolithos <neolithos@outlook.com>
2019-02-28 17:16:57 +0100

    WIP: Link Editor

---
+++ PPSnDesktop/UI/PpsObjectLinkEditor.cs
@@ -0,0 +1,33 @@
+﻿#region -- copyright --
+//
+// Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+// European Commission - subsequent versions of the EUPL(the "Licence"); You may
+// not use this work except in compliance with the Licence.
+//
+// You may obtain a copy of the Licence at:
+// http://ec.europa.eu/idabc/eupl
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+// specific language governing permissions and limitations under the Licence.
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TecWare.PPSn.UI
+{
+	public class PpsObjectLinkEditor : Control
+	{
+		static PpsObjectLinkEditor()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(PpsObjectLinkEditor), new FrameworkPropertyMetadata(typeof(PpsObjectLinkEditor)));
+		}
+	} // class PpsObjectLinkEditor
+}
