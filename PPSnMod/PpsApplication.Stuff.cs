34d7b6f4956a8adc93d29d4b35f9205714f773b4
neolithos <neolithos@outlook.com>
2022-12-29 19:54:34 +0100

    Chg: IDataRow converter and handling

--- PPSnMod/PpsApplication.Stuff.cs
+++ PPSnMod/PpsApplication.Stuff.cs
@@ -14,17 +14,42 @@
 //
 #endregion
 using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Neo.IronLua;
 using TecWare.DE.Data;
 using TecWare.DE.Stuff;
+using TecWare.PPSn.Data;
+using TecWare.PPSn.Server.Data;
 
 namespace TecWare.PPSn.Server
 {
 	public partial class PpsApplication
 	{
+		#region -- class RowCopy ------------------------------------------------------
+
+		private sealed class RowCopy : DynamicDataRow
+		{
+			private readonly object[] values;
+			private readonly IDataColumns columns;
+
+			public RowCopy(object[] values, IDataColumns columns)
+			{
+				this.values = values ?? throw new ArgumentNullException(nameof(values));
+				this.columns = columns ?? throw new ArgumentNullException(nameof(columns));
+			} // ctor
+
+			public override object this[int index] => values[index];
+			public override IReadOnlyList<IDataColumn> Columns => columns.Columns;
+
+			public override bool IsDataOwner => true;
+		} // class RowCopy
+
+		#endregion
+
 		#region -- class RowsArray ----------------------------------------------------
 
 		private sealed class RowsArray : IReadOnlyList<IDataRow>, IDataColumns
@@ -52,49 +77,422 @@ namespace TecWare.PPSn.Server
 
 		#endregion
 
-		/// <summary>Return row of a request.</summary>
-		/// <param name="row"></param>
-		/// <param name="columnInfo"></param>
-		/// <returns></returns>
-		[LuaMember]
-		public static IDataRow GetRow(IDataRow row, IDataColumns columnInfo = null)
+		#region -- class RowsEnumerable -----------------------------------------------
+
+		private sealed class RowsEnumerable : IEnumerable<IDataRow>, IDataColumns
+		{
+			private readonly IDataColumns columns;
+			private readonly IEnumerable<IDataRow> rows;
+
+			public RowsEnumerable(IDataColumns columns, IEnumerable<IDataRow> rows)
+			{
+				this.columns = columns ?? throw new ArgumentNullException(nameof(columns));
+				this.rows = rows ?? throw new ArgumentNullException(nameof(rows));
+			} // ctor
+
+			public IEnumerator<IDataRow> GetEnumerator()
+				=> rows.GetEnumerator();
+
+			IEnumerator IEnumerable.GetEnumerator()
+				=> rows.GetEnumerator();
+
+			public IReadOnlyList<IDataColumn> Columns => columns.Columns;
+		} // class RowsEnumerable
+
+		#endregion
+
+		#region -- class RowColumn ----------------------------------------------------
+
+		private sealed class RowColumn : IPpsColumnDescription
+		{
+			private readonly string columnName;
+			private readonly IPpsColumnDescription columnDescription;
+
+			public RowColumn(string columnName, IPpsColumnDescription columnDescription)
+			{
+				this.columnName = columnName ?? throw new ArgumentNullException(nameof(columnName));
+				this.columnDescription = columnDescription ?? throw new ArgumentNullException(nameof(columnDescription));
+			} // ctor
+
+			public string Name => columnName;
+
+			public Type DataType => columnDescription.DataType ?? typeof(string);
+
+			public IPropertyEnumerableDictionary Attributes => columnDescription.Attributes;
+
+			public T GetColumnDescription<T>()
+				where T : IPpsColumnDescription
+				=> columnDescription.GetColumnDescription<T>();
+		} // class RowColumn
+
+		#endregion
+
+		#region -- class RowColumns ---------------------------------------------------
+
+		private sealed class RowColumns : IPpsColumnDescriptions
+		{
+			private readonly IPpsColumnDescription[] columns;
+
+			public RowColumns(IPpsColumnDescription[] columns)
+			{
+				this.columns = columns ?? throw new ArgumentNullException(nameof(columns));
+			} // ctor
+
+			public IReadOnlyList<IDataColumn> Columns => columns;
+
+			public IPpsColumnDescription GetFieldDescription(int columnIndex)
+				=> columns[columnIndex];
+
+			public IPpsColumnDescription GetFieldDescription(string columnName)
+				=> columns.FirstOrDefault(c => String.Compare(c.Name, columnName, StringComparison.OrdinalIgnoreCase) == 0);
+		} // class RowColumns
+
+		#endregion
+
+		#region -- GetRow -------------------------------------------------------------
+
+		private static IDataRow GetRowWithColumnInfo(IDataRow row, IDataColumns columnInfo)
+		{
+			var values = new object[row.Columns.Count];
+			for (var i = 0; i < values.Length; i++)
+				values[i] = row[i];
+
+			return new RowCopy(values, columnInfo);
+		} // GetRowWithColumnInfo
+
+		private static IDataRow GetRowCreateColumnInfo(IDataRow row)
 		{
 			var columns = new IDataColumn[row.Columns.Count];
 			var values = new object[row.Columns.Count];
 			for (var i = 0; i < values.Length; i++)
 			{
 				values[i] = row[i];
-				columns[i] = (columnInfo ?? row).Columns[i];
+					columns[i] = row.Columns[i];
 			}
 
-			return new SimpleDataRow(values, columns);
-		} // GetRow
+			return new RowCopy(values, new SimpleDataColumns(columns));
+		} // GetRowCreateColumnInfo
+
+		/// <summary>Return row of a request.</summary>
+		/// <param name="row"></param>
+		/// <param name="columnInfo"></param>
+		/// <returns></returns>
+		[LuaMember]
+		public static IDataRow GetRow(IDataRow row, IDataColumns columnInfo = null)
+			=> columnInfo == null ? GetRowCreateColumnInfo(row) : GetRowWithColumnInfo(row, columnInfo);
+
+		/// <summary>Create a copy of the object as an row.</summary>
+		/// <param name="values"></param>
+		/// <param name="columnInfo"></param>
+		/// <returns></returns>
+		[LuaMember]
+		public static IDataRow GetRowFromGeneric(IDataColumns columnInfo, object values)
+		{
+			switch (values)
+			{
+				case IDataRow row:
+					return GetRow(row, columnInfo);
+				case LuaTable table:
+					return GetRowFromTable(columnInfo, table);
+				case IPropertyReadOnlyDictionary properties:
+					return GetRowFromProperties(columnInfo, properties);
+				default:
+					object tmp = null;
+					return GetRowFromObject(columnInfo, values, ref tmp);
+			}
+		} // func GetRowFromGeneric
+
+		/// <summary>Create a copy of the table as an row.</summary>
+		/// <param name="table"></param>
+		/// <param name="columnInfo"></param>
+		/// <returns></returns>
+		[LuaMember]
+		public static IDataRow GetRowFromTable(IDataColumns columnInfo, LuaTable table)
+		{
+			var values = new object[columnInfo.Columns.Count];
+			for (var i = 0; i < values.Length; i++)
+				values[i] = table.GetMemberValue(columnInfo.Columns[i].Name);
+			return new RowCopy(values, columnInfo);
+		} // func GetRowFromTable
+
+		/// <summary>Create a copy of the properties as an row.</summary>
+		/// <param name="properties"></param>
+		/// <param name="columnInfo"></param>
+		/// <returns></returns>
+		[LuaMember]
+		public static IDataRow GetRowFromProperties(IDataColumns columnInfo, IPropertyReadOnlyDictionary properties)
+		{
+			var values = new object[columnInfo.Columns.Count];
+			for (var i = 0; i < values.Length; i++)
+				values[i] = properties.TryGetProperty(columnInfo.Columns[i].Name, out var v) ? v : null;
+			return new RowCopy(values, columnInfo);
+		} // func GetRowFromTable
+
+		/// <summary>Create a copy of the object as an row.</summary>
+		/// <param name="obj"></param>
+		/// <param name="columnInfo"></param>
+		/// <param name="infoCache"></param>
+		/// <returns></returns>
+		[LuaMember]
+		public static IDataRow GetRowFromObject(IDataColumns columnInfo, object obj, ref object infoCache)
+		{
+			if (obj is null)
+				throw new ArgumentNullException(nameof(obj));
+
+			// build member info
+			if (!(infoCache is MemberInfo[] memberInfo))
+			{
+				var typeInfo = obj.GetType();
+				memberInfo = new MemberInfo[columnInfo.Columns.Count];
+				for (var i = 0; i < memberInfo.Length; i++)
+					memberInfo[i] = typeInfo.GetMember(columnInfo.Columns[i].Name).FirstOrDefault();
+			}
+
+			// copy values
+			var values = new object[memberInfo.Length];
+			for (var i = 0; i < values.Length; i++)
+			{
+				switch (memberInfo[i])
+				{
+					case FieldInfo fieldInfo:
+						values[i] = fieldInfo.GetValue(obj);
+						break;
+					case PropertyInfo propertyInfo:
+						values[i] = propertyInfo.GetValue(obj);
+						break;
+					case MethodInfo methodInfo:
+						values[i] = methodInfo.Invoke(obj, Array.Empty<object>());
+						break;
+					default:
+						values[i] = null;
+						break;
+				}
+			}
+
+			return new RowCopy(values, columnInfo);
+		} // func GetRowFromObject
+
+		#endregion
+
+		#region -- GetRows, CopyRows --------------------------------------------------
+
+		private static IEnumerable<IDataRow> CopyRowsCore(IEnumerable<IDataRow> rows, IDataColumns columnInfo)
+		{
+			var isFirst = true;
+			foreach (var row in rows)
+			{
+				if (isFirst)
+				{
+					isFirst = false;
+
+					if (columnInfo == null) // copy columns of the first row
+					{
+						var firstRow = GetRowCreateColumnInfo(row);
+						columnInfo = firstRow;
+						yield return firstRow;
+					}
+					else // validate columnInfo
+					{
+						if (row.Columns.Count != columnInfo.Columns.Count)
+							throw new ArgumentOutOfRangeException($"ColumnInfo does not the same number of columns (columnInfo={columnInfo.Columns.Count},row={row.Columns.Count}");
+						yield return GetRowWithColumnInfo(row, columnInfo);
+					}
+				}
+				else
+					yield return GetRowWithColumnInfo(row, columnInfo);
+			}
+		} // func CopyRowsCore
 
 		/// <summary>Copy single rows.</summary>
 		/// <param name="rows"></param>
+		/// <param name="columnInfo"></param>
 		/// <returns></returns>
 		[LuaMember]
-		public static IEnumerable<IDataRow> CopyRows(IEnumerable<IDataRow> rows)
-			=> rows.Select(row => GetRow(row, rows as IDataColumns));
+		public static IEnumerable<IDataRow> CopyRows(IEnumerable<IDataRow> rows, IDataColumns columnInfo = null)
+			=> columnInfo == null ? CopyRowsCore(rows, columnInfo) : new RowsEnumerable(columnInfo, CopyRowsCore(rows, columnInfo));
 
 		/// <summary>Create a copy of the whole result set.</summary>
 		/// <param name="rows"></param>
 		/// <param name="offset"></param>
 		/// <param name="count"></param>
+		/// <param name="columnInfo"></param>
 		/// <returns></returns>
 		[LuaMember]
-		public static IReadOnlyList<IDataRow> GetRows(IEnumerable<IDataRow> rows, int offset = 0, int count = Int32.MaxValue)
+		public static IReadOnlyList<IDataRow> GetRows(IEnumerable<IDataRow> rows, int offset = 0, int count = Int32.MaxValue, IDataColumns columnInfo = null)
 		{
-			var columns = rows as IDataColumns;
+			rows = CopyRows(rows, columnInfo);
 
-			rows = CopyRows(rows);
 			if (offset > 0)
 				rows = rows.Skip(offset);
 			if (count < Int32.MaxValue)
 				rows = rows.Take(count);
 
-			return new RowsArray(columns, rows.ToArray());
+			return new RowsArray(columnInfo ?? rows as IDataColumns, rows.ToArray());
 		} // func GetRows
+
+		/// <summary>Create a copy of the whole result set.</summary>
+		/// <param name="rows"></param>
+		/// <param name="offset"></param>
+		/// <param name="count"></param>
+		/// <param name="columnInfo"></param>
+		/// <returns></returns>
+		[LuaMember]
+		public IReadOnlyList<IDataRow> GetRowsWithFields(IEnumerable<IDataRow> rows, int offset = 0, int count = Int32.MaxValue, object columnInfo = null)
+			=> GetRows(rows, offset, count, CreateColumnDescriptions(columnInfo));
+
+		#endregion
+
+		#region -- CreateColumnDescriptions -------------------------------------------
+
+		private IPpsColumnDescription CreateColumnDescription(string fieldDescription, string name)
+		{
+			if (String.IsNullOrEmpty(name))
+			{
+				var p = fieldDescription.LastIndexOf('.');
+				name = p >= 0 ? fieldDescription.Substring(p) : fieldDescription; ;
+			}
+			return new RowColumn(name, GetFieldDescription(fieldDescription));
+		} // func CreateColumnDescription
+
+		/// <summary>Create a set of columns.</summary>
+		/// <param name="fieldDescriptions">Alias is allowed with the equal seperator.</param>
+		/// <returns></returns>
+		[LuaMember]
+		public IPpsColumnDescriptions CreateColumnDescriptionsFromFields(params string[] fieldDescriptions)
+		{
+			var columnInfo = new List<IPpsColumnDescription>();
+			foreach (var cur in fieldDescriptions)
+			{
+				var p = cur.IndexOf('=');
+				if (p >= 0)
+					columnInfo.Add(CreateColumnDescription(cur.Substring(p + 1), cur.Substring(0, p)));
+				else
+					columnInfo.Add(CreateColumnDescription(cur, null));
+			}
+			return new RowColumns(columnInfo.ToArray());
+		} // func CreateColumnDescriptions
+
+		/// <summary>Create a set of columns.</summary>
+		/// <param name="descriptions">Index by index.</param>
+		/// <returns></returns>
+		[LuaMember]
+		public IPpsColumnDescriptions CreateColumnDescriptionsFromTable(LuaTable descriptions)
+		{
+			var columnInfo = new List<IPpsColumnDescription>();
+			foreach(var kv in descriptions.Members)
+			{
+				if (kv.Value is string fieldDescription)
+					columnInfo.Add(CreateColumnDescription(fieldDescription, kv.Key));
+				else
+					throw new ArgumentException($"Invalid column definition for '{kv.Key}'");
+			}
+			return new RowColumns(columnInfo.ToArray());
+		} // func CreateColumnDescriptionsFromTable
+
+		/// <summary>Create a set of columns.</summary>
+		/// <param name="descriptions">Alias is allowed with the equal seperator.</param>
+		/// <returns></returns>
+		[LuaMember]
+		public IPpsColumnDescriptions CreateColumnDescriptions(object descriptions)
+		{
+			if (descriptions is IPpsColumnDescriptions columns)
+				return columns;
+			else if (descriptions is string[] fieldList)
+				return CreateColumnDescriptionsFromFields(fieldList);
+			else if (descriptions is LuaTable fieldTable)
+				return CreateColumnDescriptionsFromTable(fieldTable);
+			else
+				throw new ArgumentException("Unsupported type.", nameof(descriptions));
+		} // func CreateColumnDescriptions
+
+		private static IDataRow CreateRowsCopyFromObject(IDataColumns columnInfo, object o, IDictionary<Type, object> infoCaches)
+		{
+			switch (o)
+			{
+				case LuaTable t:
+					return GetRowFromTable(columnInfo, t);
+				case IPropertyReadOnlyDictionary p:
+					return GetRowFromProperties(columnInfo, p);
+				case null:
+					return new RowCopy(new object[columnInfo.Columns.Count], columnInfo);
+				default:
+					var typeInfo = o.GetType();
+					if (infoCaches.TryGetValue(typeInfo, out var ic))
+						return GetRowFromObject(columnInfo, o, ref ic);
+					else
+					{
+						ic = null;
+						var r = GetRowFromObject(columnInfo, o, ref ic);
+						infoCaches[typeInfo] = ic;
+						return r;
+					}
+			}
+		} // func CreateRowsCopyFromObject
+
+		#endregion
+
+		#region -- CreateRows ---------------------------------------------------------
+
+		private static IEnumerable<IDataRow> CreateRowsGeneric(IDataColumns columnInfo, IEnumerable e)
+		{
+			var infoCaches = new Dictionary<Type, object>();
+			foreach (var c in e)
+				yield return CreateRowsCopyFromObject(columnInfo, c, infoCaches);
+		} // func CreateRowsGeneric
+
+		private static IEnumerable<IDataRow> CreateRowsFromFunction(IDataColumns columnInfo, object func)
+		{
+			var infoCaches = new Dictionary<Type, object>();
+			while (true)
+			{
+				var r = new LuaResult(Lua.RtInvoke(func));
+				var o = r[0];
+				if (o == null)
+					break;
+				yield return CreateRowsCopyFromObject(columnInfo, o, infoCaches);
+			}
+		} // func CreateRowsFromFunction
+
+		/// <summary></summary>
+		/// <param name="columnInfo"></param>
+		/// <param name="rowInfo"></param>
+		/// <param name="enforceCopy"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException"></exception>
+		[LuaMember]
+		public IEnumerable<IDataRow> CreateRows(object columnInfo, object rowInfo, bool enforceCopy)
+		{
+			var columns = CreateColumnDescriptions(columnInfo);
+
+			if (rowInfo == null)
+				return new RowsArray(columns, Array.Empty<IDataRow>());
+			else
+			{
+				IEnumerable<IDataRow> rowEnum;
+				if (rowInfo is LuaTable tableArray)
+					rowEnum = tableArray.ArrayList.Select(c => GetRowFromGeneric(columns, c));
+				else if (rowInfo is IEnumerable<LuaTable> tableEnum)
+					rowEnum = tableEnum.Select(t => GetRowFromTable(columns, t));
+				else if (rowInfo is IEnumerable<IPropertyReadOnlyDictionary> propertyEnum)
+					rowEnum = propertyEnum.Select(p => GetRowFromProperties(columns, p));
+				else if (rowInfo is IEnumerable genericEnum)
+					rowEnum = CreateRowsGeneric(columns, genericEnum);
+				else if (Lua.RtInvokeable(rowInfo))
+					rowEnum = CreateRowsFromFunction(columns, rowInfo);
+				else
+					throw new ArgumentException("Unknown typ for rowInfo.", nameof(rowInfo));
+
+				// create result
+				if (enforceCopy)
+					return new RowsArray(columns, rowEnum.ToArray());
+				else
+					return new RowsEnumerable(columns, rowEnum);
+			}
+		} // func CreateRows
+
+		#endregion
+
+		#region -- GetFirstRow --------------------------------------------------------
 
 		/// <summary>Return first row of a request.</summary>
 		/// <param name="rows"></param>
@@ -102,6 +500,10 @@ namespace TecWare.PPSn.Server
 		[LuaMember]
 		public static IDataRow GetFirstRow(IEnumerable<IDataRow> rows)
 			=> CopyRows(rows).FirstOrDefault();
+
+		#endregion
+
+		#region -- GetTable -----------------------------------------------------------
 
 		private static LuaTable GetTableCore(IDataRow row)
 		{
@@ -188,6 +590,10 @@ namespace TecWare.PPSn.Server
 			return t;
 		} // func GetTableWithRows
 
+		#endregion
+
+		#region -- GetProperty --------------------------------------------------------
+
 		/// <summary>Get a structured property.</summary>
 		/// <param name="value"></param>
 		/// <param name="propertyPath"></param>
@@ -243,5 +649,7 @@ namespace TecWare.PPSn.Server
 				}
 			}
 		} // func GetTableProperty
+
+		#endregion
 	} // class PpsApplication
 }
