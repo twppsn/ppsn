7260e4c783bf5b15a0379d01692eafe5677d44c2
a.meyer <a.meyer@tecware-gmbh.de>
2026-02-23 08:49:42 +0100

    Add: PpsCalendar

---
+++ PPSnDesktopUI/Controls/PpsCalendar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TecWare.PPSn.Controls
+{
+	#region -- class PpsCalendar -------------------------------------------------------
+	class PpsCalendar : Calendar, IPpsNullableControl
+	{
+		#region -- Ctor ----------------------------------------------------------------
+		public PpsCalendar() 
+		{
+			SelectionMode = CalendarSelectionMode.MultipleRange; // Wir wollen alle Urlaubsblöcke anzeigen
+		} // ctor
+		#endregion
+
+		#region -- IsNullable Property -------------------------------------------------
+		public bool CanClear => IsEnabled;
+
+		public bool IsNullable => true;
+
+		public void Clear()
+		{
+			BlackoutDates.Clear();
+			if (SelectedDate.HasValue)
+				SelectedDate = null;
+			SelectedDates.Clear();
+		} // proc Clear
+		#endregion
+	} // class PpsCalendar
+	#endregion
+}
