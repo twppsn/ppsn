df7119c4f65df24d818053542603f40e5c3c14aa
a.meyer <a.meyer@tecware-gmbh.de>
2026-05-08 08:30:05 +0200

    Chg: PpsVirtualizationStackPanel now works properly.

--- PPSnDesktopUI/Controls/PpsVirtualizationStackPanel.cs
+++ PPSnDesktopUI/Controls/PpsVirtualizationStackPanel.cs
@@ -14,10 +14,6 @@
 //
 #endregion
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -35,9 +31,17 @@ namespace TecWare.PPSn.Controls
 		private Size extent;
 		private Size viewPort;
 
+		private Size itemSize = new Size (100, 30);
+
 		private ItemContainerGenerator generator = null;
 
-		private IItemContainerGenerator GetItemContainerGenerator()
+		#region --- ctor --------------------------------------------------------------
+		public PpsVirtualizationStackPanel() 
+		{
+		} // ctor
+		#endregion
+
+		private IRecyclingItemContainerGenerator GetItemContainerGenerator()
 		{
 			if (generator is null)
 			{
@@ -52,63 +56,95 @@ namespace TecWare.PPSn.Controls
 
 		protected override Size MeasureOverride(Size availableSize)
 		{
-			extent = new Size(200, 30000);
+			// Wir wollen die vorhandene Fläche ausfüllen
+
+			var c = ItemsControl.GetItemsOwner(this);
+			if (c == null) return availableSize;
+
+			extent = new Size(itemSize.Width, c.Items.Count * itemSize.Height); // TODO: ItemSize dynamisch berechnen 
 			viewPort = availableSize;
+
+			var visibleCount = (int)Math.Ceiling(availableSize.Height / itemSize.Height); // Anzahl der sichtbaren Elemente
+
+			var firstVisibleIndex = GetFirstVisibleIndex(); 
+			var lastVisibleIndex = Math.Min(firstVisibleIndex + visibleCount, c.Items.Count) -1;
+
 			var generator = GetItemContainerGenerator();
-			var p = generator.GeneratorPositionFromIndex(0);
-			using (generator.StartAt(p, GeneratorDirection.Forward))
+			var startingPosition = generator.GeneratorPositionFromIndex(firstVisibleIndex); // Wir rendern erst ab dem ersten sichtbaren Index
+			var iChildIndex = (startingPosition.Offset == 0) ? startingPosition.Index : startingPosition.Index + 1;
+
+			using (generator.StartAt(startingPosition, GeneratorDirection.Forward, true))
 			{
-				var t = generator.GenerateNext(out var isNew);
-				if (t is FrameworkElement m)
+
+				for (var i = firstVisibleIndex; i <= lastVisibleIndex; i++, iChildIndex ++) // Nur sichbare Elemente bearbeiten 
 				{
+					var child = (UIElement)generator.GenerateNext(out var isNew);
 					if (isNew)
 					{
-						generator.PrepareItemContainer(m);
-						AddInternalChild(m);
-					}
-					m.Measure(availableSize);
-					var sz = m.DesiredSize;
-					return availableSize; // m.DesiredSize;
+						if (iChildIndex >= InternalChildren.Count)
+							AddInternalChild(child); // Neu erstellten Container hinzufügen
+						else
+							InsertInternalChild(iChildIndex, child); // Neu erstellten Container an dem angegebenen Index einfügen
 
+						ItemContainerGenerator.PrepareItemContainer(child); // Container Vorbereiten
+					}
+					else if (!InternalChildren.Contains(child))
+					{
+						InsertInternalChild(iChildIndex, child); // Container an dem angegebenen Index einfügen
+						generator.PrepareItemContainer(child); // Container Vorbereiten
+					}
+
+					child.Measure(availableSize); // Gewünschte Größe des Elements wird ermittelt 
+
+					if( i == firstVisibleIndex )
+					{
+						itemSize = child.DesiredSize; // Die erwünschte Größe unseres Kindelementes ist ab jetzt unsere Itemgröße 
+						ScrollVelocity = (int)Math.Ceiling(child.DesiredSize.Height); // Die aufgerundete Höhe unseres ersten Kindelements ist unsere Scrollgeschwindigkeit
+					}
 				}
+
 			}
-			return availableSize; // new Size(0, 0);
-			
-			//else
-			//	return base.MeasureOverride(availableSize);
+
+			CleanupItems(firstVisibleIndex, lastVisibleIndex); // Nicht sichtbares Zeug aufräumen
+
+			return new Size(
+				double.IsInfinity(availableSize.Width) ? 0 : availableSize.Width, 
+				double.IsInfinity(availableSize.Height) ? 0 : availableSize.Height
+			);
 		} // func
 
 		protected override Size ArrangeOverride(Size finalSize)
 		{
-				var generator = GetItemContainerGenerator();
-			var p = generator.GeneratorPositionFromIndex(0);
-			var rc = new Rect(0, 0, finalSize.Width, 30);
-			using (generator.StartAt(p, GeneratorDirection.Forward))
+			double y = 0;
+			foreach(UIElement child in InternalChildren) // Für jedes interne Kindelement
 			{
-				while (true)
-				{
-					var t = generator.GenerateNext(out var isNew);
-					if (t is FrameworkElement m)
-					{
-						if (isNew)
-						{
-							generator.PrepareItemContainer(m);
-							AddInternalChild(m);
-						}
-						m.Arrange(rc);
-						rc.Y += 30;
-					}
-					else
-						break;
-				}
+				y = (GetItemIndexFromChild(child) * itemSize.Height) - offset.Y;
+				child.Arrange(new Rect(new Point(0, y), child.DesiredSize)); // Positioniert das Kindelement
 			}
 			
 			viewPort = finalSize;
-			return base.ArrangeOverride(finalSize);
+			return finalSize;
 		}
 
-		//protected override double GetItemOffsetCore(UIElement child) 
-		//	=> base.GetItemOffsetCore(child);
+		private void CleanupItems(int minVisibleIndex, int maxVisibleIndex)
+		{
+			var generator = (IRecyclingItemContainerGenerator)ItemContainerGenerator;
+
+			for (var i = InternalChildren.Count - 1; i >= 0; i--)
+			{
+				var childPos = new GeneratorPosition(i, 0);
+				var itemIndex = generator.IndexFromGeneratorPosition(childPos);
+
+				if (itemIndex < minVisibleIndex || itemIndex > maxVisibleIndex)
+				{
+					generator.Recycle(childPos, 1);
+					RemoveInternalChildRange(i, 1);
+				}
+			}
+		} // proc CleanupItems
+
+		protected override double GetItemOffsetCore(UIElement child) 
+			=> base.GetItemOffsetCore(child);
 
 		//protected override void OnItemsChanged(object sender, ItemsChangedEventArgs args) 
 		//	=> base.OnItemsChanged(sender, args);
@@ -130,20 +166,112 @@ namespace TecWare.PPSn.Controls
 		double IScrollInfo.ExtentWidth => extent.Width;
 		double IScrollInfo.ExtentHeight => extent.Height;
 
-		void IScrollInfo.LineDown() => throw new NotImplementedException();
-		void IScrollInfo.LineLeft() => throw new NotImplementedException();
-		void IScrollInfo.LineRight() => throw new NotImplementedException();
-		void IScrollInfo.LineUp() => throw new NotImplementedException();
+		void IScrollInfo.LineDown()
+		{
+			SetVerticalOffsetCore(offset.Y + ScrollVelocity);
+		} // proc IScrollInfo.LineDown()
+
+		void IScrollInfo.LineLeft()
+		{
+			SetHorizontalOffsetCore(offset.X - ScrollVelocity);
+		} // proc IScrollInfo.LineLeft()
+
+		void IScrollInfo.LineRight()
+		{
+			SetHorizontalOffsetCore(offset.X + ScrollVelocity);
+		} // proc IScrollInfo.LineRight()
+
+		void IScrollInfo.LineUp()
+		{
+			SetVerticalOffsetCore(offset.Y - ScrollVelocity);
+		} // proc IScrollInfo.LineUp
 		Rect IScrollInfo.MakeVisible(Visual visual, Rect rectangle) => new Rect(0, 0, 0, 0);
-		void IScrollInfo.MouseWheelDown() => throw new NotImplementedException();
-		void IScrollInfo.MouseWheelLeft() => throw new NotImplementedException();
-		void IScrollInfo.MouseWheelRight() => throw new NotImplementedException();
-		void IScrollInfo.MouseWheelUp() => throw new NotImplementedException();
-		void IScrollInfo.PageDown() => throw new NotImplementedException();
-		void IScrollInfo.PageLeft() => throw new NotImplementedException();
-		void IScrollInfo.PageRight() => throw new NotImplementedException();
-		void IScrollInfo.PageUp() => throw new NotImplementedException();
-		void IScrollInfo.SetHorizontalOffset(double offset) => throw new NotImplementedException();
-		void IScrollInfo.SetVerticalOffset(double offset) => throw new NotImplementedException();
+
+		void IScrollInfo.MouseWheelDown()
+		{
+			SetVerticalOffsetCore(offset.Y + ScrollVelocity);
+		} // proc IScrollInfo.MouseWheelDown
+
+		void IScrollInfo.MouseWheelLeft()
+		{
+			SetHorizontalOffsetCore(offset.X - ScrollVelocity);
+		} // proc IScrollInfo.MouseWheelLeft
+
+		void IScrollInfo.MouseWheelRight()
+		{
+			SetHorizontalOffsetCore(offset.X + ScrollVelocity);
+		} // proc IScrollInfo.MouseWheelRight
+
+		void IScrollInfo.MouseWheelUp()
+		{
+			SetVerticalOffsetCore(offset.Y - ScrollVelocity);
+		} // proc IScrollInfo.MouseWheelUp()
+
+		void IScrollInfo.PageDown()
+		{
+			SetVerticalOffsetCore(offset.Y + viewPort.Height);
+		} // proc IScrollInfo.PageDown
+
+		void IScrollInfo.PageLeft()
+		{
+			SetHorizontalOffsetCore(offset.X - viewPort.Width);
+		} // proc IScrollInfo.PageLeft
+
+		void IScrollInfo.PageRight()
+		{
+			SetHorizontalOffsetCore(offset.X + viewPort.Width);
+		} // proc IScrollInfo.PageRight
+
+		void IScrollInfo.PageUp()
+		{
+			SetVerticalOffsetCore(offset.Y - viewPort.Height);
+		} // proc IScrollInfo.PageUp
+
+		void IScrollInfo.SetHorizontalOffset(double offset)
+		{
+			SetHorizontalOffsetCore(offset);
+		} // proc IScrollInfo.SetHorizontalOffset
+
+		void IScrollInfo.SetVerticalOffset(double offset)
+		{
+			SetVerticalOffsetCore(offset);
+		} // proc IScrollInfo.SetVerticalOffset
+
+		#region --- Core Methods ------------------------------------------------------
+
+		private void SetVerticalOffsetCore(double offset) 
+		{
+			this.offset.Y = Math.Max(0, Math.Min(offset, extent.Height - viewPort.Height));
+
+			// Notify that layout has changed
+			scrollViewer.InvalidateScrollInfo();
+			InvalidateMeasure();
+		} // proc SetVerticalOffsetCore
+
+		private void SetHorizontalOffsetCore(double offset)
+		{
+			this.offset.X = Math.Max(0, Math.Min(offset, extent.Width - viewPort.Width));
+
+			// Notify that layout has changed
+			scrollViewer.InvalidateScrollInfo();
+			InvalidateMeasure();
+		} // proc SetHorizontalOffsetCore
+
+		private int GetFirstVisibleIndex() 
+		{
+			var index = (int)Math.Floor(offset.Y / itemSize.Height);
+			return Math.Max(0, index);
+		} // proc GetFirstVisibleIndex
+
+		private int GetItemIndexFromChild(UIElement child) 
+		{
+			var c = ItemsControl.GetItemsOwner(this);
+			return c.ItemContainerGenerator.IndexFromContainer(child);
+		} // proc GetItemIndexFromChild
+
+		#endregion
+
+		private int ScrollVelocity = 0;
+
 	} // class PpsVirtualizationStackPanel
 }
