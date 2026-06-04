6e68985d5e56c9c16cbb44fd97cb4a6efe99e0d2
a.meyer <a.meyer@tecware-gmbh.de>
2026-06-04 15:11:11 +0200

    Chg: Reworked and expanded PpsDragAdorner.

--- PPSnDesktopUI/UI/PpsDragAdorner.cs
+++ PPSnDesktopUI/UI/PpsDragAdorner.cs
@@ -7,44 +7,59 @@ namespace TecWare.PPSn.UI
 {
 	public class PpsDragAdorner : Adorner
 	{
-		private readonly UIElement adornerElement;
-		private AdornerLayer adornerLayer;
+		private readonly UIElement dragElement;
+		private readonly VisualBrush brush;
 		private Point offset;
 
-		public PpsDragAdorner(UIElement adornedElement, UIElement adornerElement, Point offset) : base(adornedElement)
+		/// <param name="adornedElement">The element the adorner is bound to.</param>
+		/// <param name="dragElement">The element we want to drag & drop.</param>
+		/// <param name="opacity">The opacity of the dragElement.</param>
+		public PpsDragAdorner(UIElement adornedElement, UIElement dragElement, float opacity) : base(adornedElement)
 		{
-			this.adornerElement = adornerElement;
-			this.offset = offset;
+			this.dragElement = dragElement;
 
-			adornerLayer = AdornerLayer.GetAdornerLayer(adornedElement);
+			brush = new VisualBrush(adornedElement)
+			{
+				Opacity = opacity
+
+			};
 
 			// Das Vorschaubild ist für Mausereignisse unsichtbar 
-			adornerElement.IsHitTestVisible = false;
+			dragElement.IsHitTestVisible = false;
 		} // ctor
 
 		#region -- Drawing Overrides --------------------------------------------------
 		protected override Size MeasureOverride(Size constraint)
 		{
-			adornerElement.Measure(constraint);
-			return adornerElement.DesiredSize;
+			dragElement.Measure(constraint);
+			return dragElement.DesiredSize;
 		} // proc MeasureOverride
 
 		protected override Size ArrangeOverride(Size finalSize) 
 		{
 			// Laut Dr. Google sollte die Positionierung hier und nicht im OnRender stattfinden 
-			adornerElement.Arrange(new Rect(offset, adornerElement.DesiredSize));
+			dragElement.Arrange(new Rect(offset, dragElement.DesiredSize));
 			return finalSize;
 		} // proc ArrangeOverride
 
 		protected override void OnRender(DrawingContext drawingContext)
 		{
-			// Sollten wir hier einen RenderBrush definieren oder dies dem Adorner-Nutzer überlassen? 
+			var size = dragElement.RenderSize;
+			var rect = new Rect(offset.X, offset.Y, size.Width, size.Height);
+			drawingContext.DrawRectangle(brush, null, rect); // Ist das so korrekt? 
 		} // proc OnRender
 
 		#endregion
 
+		/// <summary>Set new Offset (position) and invalidate visual.</summary>
+		public void SetOffset(Point newOffset)
+		{
+			offset = newOffset;
+			InvalidateVisual();
+		} // proc SetOffset
+
 		protected override int VisualChildrenCount => 1;
 
-		protected override Visual GetVisualChild(int index) => adornerElement;
+		protected override Visual GetVisualChild(int index) => dragElement;
 	} // class PpsDragAdorner
 }
